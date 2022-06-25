431e593c38e5354777eebba574669e97fe42a2be
Wynne-Tran <wynnetran2018@gmail.com>
2022-06-24 21:10:43 -0400

    Todo Application with Kotlin

---
+++ app/src/main/java/dandelion/mobile/myapplication/MainActivity.kt
@@ -0,0 +1,33 @@
+package dandelion.mobile.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+
+        rvTodoItem.adapter = todoAdapter
+        rvTodoItem.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteTodo.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+}
---
+++ app/src/main/java/dandelion/mobile/myapplication/Todo.kt
@@ -0,0 +1,6 @@
+package dandelion.mobile.myapplication
+
+data class Todo (
+    val title: String,
+    var isChecked: Boolean = false
+        )
